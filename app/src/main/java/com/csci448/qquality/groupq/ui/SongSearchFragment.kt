fd0c1fe8bfed2306ba194ec2ea91d891aeb500b1
Nick Klonne <nklonne@mines.edu>
2020-02-25 22:17:26 -0700

    Began implementing SongSearchFragment...at least inflated the view

--- app/src/main/java/com/csci448/qquality/groupq/ui/SongSearchFragment.kt
+++ app/src/main/java/com/csci448/qquality/groupq/ui/SongSearchFragment.kt
@@ -1,7 +1,36 @@
 package com.csci448.qquality.groupq.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.csci448.qquality.groupq.R
 
 class SongSearchFragment : Fragment() {
     // TODO nick implement this and layout
+
+    private lateinit var searchRecyclerView: RecyclerView
+
+    private lateinit var searchButton: Button
+    private lateinit var searchEditText: EditText
+    private lateinit var sourceSpinner: Spinner
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_song_search, container, false)
+
+        searchButton = view.findViewById(R.id.search_button)
+        searchEditText = view.findViewById(R.id.search_edit_text)
+        sourceSpinner = view.findViewById(R.id.source_spinner)
+
+        return view
+    }
 }
