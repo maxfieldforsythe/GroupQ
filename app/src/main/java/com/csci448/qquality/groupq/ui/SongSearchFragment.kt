891c168dedcd268ad391147f2c7fb0d10ab9cffa
Nick Klonne <nklonne@mines.edu>
2020-02-27 11:14:46 -0700

    Added incomplete adapter and viewholder inner classes for song search recyclerview

--- app/src/main/java/com/csci448/qquality/groupq/ui/SongSearchFragment.kt
+++ app/src/main/java/com/csci448/qquality/groupq/ui/SongSearchFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Spinner
+import android.widget.*
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.csci448.qquality.groupq.R
@@ -30,7 +28,38 @@ class SongSearchFragment : Fragment() {
         searchButton = view.findViewById(R.id.search_button)
         searchEditText = view.findViewById(R.id.search_edit_text)
         sourceSpinner = view.findViewById(R.id.source_spinner)
+        searchRecyclerView = view.findViewById(R.id.song_seach_recycler)
 
         return view
     }
+
+
+    // Inner class view holder
+    private inner class SongHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        // TODO implement ViewHolder
+
+        val titleTextView: TextView = itemView.findViewById(R.id.song_title)
+        val artistTextView: TextView = itemView.findViewById(R.id.song_artist)
+        val addButton: Button = itemView.findViewById(R.id.add_button)
+
+        // TODO bind the data
+        //fun bind(songData)
+    }
+
+
+    private inner class SongSearchAdapter() :
+            RecyclerView.Adapter<SongHolder>() {
+
+        // TODO implement adapter fully
+        // TODO override fun getItemCount() : Int {}
+        // TODO override fun onBindViewHolder(holder: SongHolder, position: Int) {}
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SongHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.list_item_song_search, parent, false)
+            return SongHolder(view)
+        }
+
+    }
+
 }
