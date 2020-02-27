2978f105a2a480600e3fd60c9cf00449384927f5
Nick Klonne <nklonne@mines.edu>
2020-02-27 11:20:07 -0700

    Added barebones SongSearchResult Class

---
+++ app/src/main/java/com/csci448/qquality/groupq/data/SongSearchResult.kt
@@ -0,0 +1,11 @@
+package com.csci448.qquality.groupq.data
+
+/*
+* This class is currently intended to hold the data for songs in a search query.
+*   Depending on how the rest of the app is implemented, it may be useful to
+*  rename the class and use one song (data?) class in all uses of song, but
+*  for now it is called SongSearchResult because that si where it is being used
+ */
+data class SongSearchResult(var title: String, var artist: String )
+// TODO add url field or other way to actually get the song data
+// TODO there may have to be separate types of songs for youtube and soundcloud sources. TDB
