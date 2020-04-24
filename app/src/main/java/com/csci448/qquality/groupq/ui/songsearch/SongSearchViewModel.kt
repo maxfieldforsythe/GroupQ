81f13adc6094dd72ae49b8e81f0e2cc6d7d8a9ab
Nick Klonne <nklonne@mines.edu>
2020-04-24 09:48:53 -0600

    very minimal work to start implementing database funct. into song search fragment

--- app/src/main/java/com/csci448/qquality/groupq/ui/songsearch/SongSearchViewModel.kt
+++ app/src/main/java/com/csci448/qquality/groupq/ui/songsearch/SongSearchViewModel.kt
@@ -2,8 +2,12 @@ package com.csci448.qquality.groupq.ui.songsearch
 
 import androidx.lifecycle.ViewModel
 import com.csci448.qquality.groupq.data.SongSearchResult
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class SongSearchViewModel() : ViewModel(){
+
+    private val database = Firebase.firestore
 
     val songs = mutableListOf<SongSearchResult>()
 
@@ -11,5 +15,10 @@ class SongSearchViewModel() : ViewModel(){
         for (i in 0 until 25) {
             songs.add(SongSearchResult("Song $i", "Artist ${i/2}"))
         }
+    }
+
+    // Function to add a song to the queue in the database
+    fun addToQueue(lobbyName: String) {
+        // TODO add to database
     }
 }
