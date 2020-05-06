00a09154ad8aa4d255db9244a22be370440eceaa
Clara Larson <calarson@mymail.mines.edu>
2020-05-06 16:26:15 -0600

    Added network connection toast

--- app/src/main/java/com/csci448/qquality/groupq/ui/login/LoginFragment.kt
+++ app/src/main/java/com/csci448/qquality/groupq/ui/login/LoginFragment.kt
@@ -1,16 +1,21 @@
 package com.csci448.qquality.groupq.ui.login
 
 import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.net.NetworkInfo
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import com.csci448.qquality.groupq.R
 import com.csci448.qquality.groupq.ui.Callbacks
+
 
 class LoginFragment : Fragment() {
 
@@ -65,6 +70,33 @@ class LoginFragment : Fragment() {
             //Toast.makeText(context, "Register pressed", Toast.LENGTH_SHORT).show()
             callbacks?.onRegisterPressed()
         }*/
+
+        if(!isNetworkAvailable(requireContext())) {
+            Toast.makeText(
+                context,
+                "Please connect to a network to use GroupQ!",
+                Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun isNetworkAvailable(context: Context): Boolean {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val nw      = connectivityManager.activeNetwork ?: return false
+            val actNw = connectivityManager.getNetworkCapabilities(nw) ?: return false
+            return when {
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                //for other device how are able to connect with Ethernet
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                //for check internet over Bluetooth
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH) -> true
+                else -> false
+            }
+        } else {
+            val nwInfo = connectivityManager.activeNetworkInfo ?: return false
+            return nwInfo.isConnected
+        }
     }
 
     // Set callbacks to null when detaching
